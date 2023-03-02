ae2dcb6cca73e7690017d1f3475c2110e2f39a2a
Cassius Vinicius <cassiusoliveira10@gmail.com>
2023-03-02 11:39:34 -0400

    Learned about templates, routes and views

--- apprecipe/models.py
+++ apprecipe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    steps = models.TextField()
+    preparation_time = models.IntegerField()
+    recipe_yield = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    recipe_date = models.DateTimeField(default=datetime.now, blank=True)
--- apprecipe/views.py
+++ apprecipe/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Recipe
 
 
 def index(request):
-    return render(request, 'index.html')
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'index.html', context)
+
+def recipe(request,pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    recipe_output = {
+        'recipe': recipe
+    }
+    return render(request, 'recipe.html', recipe_output)
     
